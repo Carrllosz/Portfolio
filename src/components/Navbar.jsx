1758c0c513cf65c3f456fad9d15a42752ddc33c4
unknown <joaocar2003@gmail.com>
2022-08-15 22:15:30 -0300

    email add

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -98,16 +98,10 @@ const Navbar = () => {
                 Github <FaGithub size={30}/>
               </a>
             </li>
-            <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
-              <a className='flex justify-between items-center w-full text-gray-300' 
-               href='/'>
-                Email <HiOutlineMail size={30}/>
-              </a>
-            </li>
             <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
               <a className='flex justify-between items-center w-full text-gray-300'
                 href={MyPDF} download='resumo.pdf'>
-                Resume <BsFillPersonLinesFill size={30}/>
+                Currículo <BsFillPersonLinesFill size={30}/>
               </a>
             </li>
           </ul>
